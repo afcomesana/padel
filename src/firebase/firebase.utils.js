55a72de61ade77278577fb6ead9285dd06b69c3b
afcomesana <48021565+afcomesana@users.noreply.github.com>
2021-07-14 16:46:45 +0200

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,14 +2,14 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const config = {
-    apiKey: "AIzaSyD4j0WzAkImqN0ub5N1cSGt_mdsd_ChGco",
-    authDomain: "padel-8ef68.firebaseapp.com",
-    databaseURL: "https://padel-8ef68-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "padel-8ef68",
-    storageBucket: "padel-8ef68.appspot.com",
-    messagingSenderId: "82197416506",
-    appId: "1:82197416506:web:f4684c72a07b846f8b4303",
-    measurementId: "G-CN3VW4XXDM"
+    apiKey: "*************************************",
+    authDomain: "padel-*******.firebaseapp.com",
+    databaseURL: "***********************************************************",
+    projectId: "padel-******",
+    storageBucket: "padel-******.appspot.com",
+    messagingSenderId: "*****************",
+    appId: "1:******************:web:**************************",
+    measurementId: "G-**************"
 };
 
 firebase.initializeApp(config);
